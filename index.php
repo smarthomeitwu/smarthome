2c18d8b089fdab2610897b2e3805486562224d59
smarthomeitwu <46618848+smarthomeitwu@users.noreply.github.com>
2019-01-12 14:05:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -52,8 +52,10 @@
               <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/666956/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
             </div>
             <div class="col-sm-4">
+              <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/666956/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
             </div>
             <div class="col-sm-4">
+              <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/666956/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
             </div>
 
       </div>
